9fa83bfccc65e52ed1f05229560fd7c6c5023333
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-09-15 17:58:45 +0900

    feat: init front

---
+++ front/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import LoginScreen from './src/LoginScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <LoginScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ front/src/LoginScreen.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {useEffect} from 'react';
+import {View, TouchableOpacity, Text, Button} from 'react-native';
+import axios from 'axios';
+import styled from 'styled-components/native';
+
+const LoginScreen = () => {
+  return (
+    <Wrapper>
+      <KakaoButton
+        onPress={() => {
+          console.log('hi');
+          axios.get('/oauth2/authorization/kakao');
+          return;
+        }}>
+        <ButtonContainer>
+          <ButtonText>카카오 로그인</ButtonText>
+        </ButtonContainer>
+      </KakaoButton>
+    </Wrapper>
+  );
+};
+
+export default LoginScreen;
+
+const KakaoButton = styled.TouchableOpacity``;
+
+const Wrapper = styled.View``;
+
+const ButtonContainer = styled.View`
+  border-radius: 8px;
+  width: 150px;
+  height: 50px;
+  background-color: yellow;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonText = styled.Text`
+  font-size: 16px;
+  font-weight: 700;
+`;
