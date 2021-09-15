6bcd868c25c8051178cc4f5820e6ff8c731f53aa
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-09-15 17:38:23 +0900

    feat: [back] token 응답 email 추가

--- back/src/main/java/com/duder/api/security/config/TokenResponse.java
+++ back/src/main/java/com/duder/api/security/config/TokenResponse.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public class TokenResponse {
     private Long id;
+    private String email;
     private String accessToken;
 
     public TokenResponse(){}
@@ -13,8 +14,19 @@ public class TokenResponse {
         return new TokenResponse(id, accessToken);
     }
 
+    public static TokenResponse of(Long id, String email, String accessToken){
+        return new TokenResponse(id, email, accessToken);
+    }
+
+
     public TokenResponse (Long id, String accessToken){
         this.id = id;
+        this.accessToken = accessToken;
+    }
+
+    public TokenResponse (Long id, String email, String accessToken){
+        this.id = id;
+        this.email = email;
         this.accessToken = accessToken;
     }
 
