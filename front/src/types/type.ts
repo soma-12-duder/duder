bade09ace5b6c86324ee7fac5c3cf2b8167f10d6
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-09-27 03:10:13 +0900

    feat: [front] navigation 타입 적용

---
+++ front/src/types/type.ts
@@ -0,0 +1,12 @@
+// navigation
+
+export type RootStackParam = {
+  HomeScreen: undefined;
+  LoginScreen: undefined;
+  DuderTab: undefined;
+};
+
+export type BottomTabParam = {
+  HomeScreen: undefined;
+  LoginScreen: undefined;
+};
