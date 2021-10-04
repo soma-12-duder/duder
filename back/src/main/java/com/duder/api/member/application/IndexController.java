48e1762a97c2dd5f1aa92ade21d8f0c56671eff0
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-04 18:23:08 +0900

    feat: [back] 토큰 인증 된 사용자를 Controller parmeter로 넘겨줌

--- back/src/main/java/com/duder/api/member/application/IndexController.java
+++ back/src/main/java/com/duder/api/member/application/IndexController.java
@@ -1,5 +1,9 @@
 package com.duder.api.member.application;
 
+import com.duder.api.member.domain.Member;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +32,9 @@ public class IndexController {
 
     @ResponseBody
     @PostMapping("/api/token")
-    public String token(){
+    public String token(@AuthenticationPrincipal OAuth2User oAuth2User){
+        System.out.println(oAuth2User.getName());
+        System.out.println((Member) oAuth2User.getAttribute("member"));
         System.out.println("token call");
         return "hello token";
     }
