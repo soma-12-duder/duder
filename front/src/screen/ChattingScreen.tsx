43c1242b7044928dca4e4ec9aff8064d26165ef2
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-07 01:30:42 +0900

    feat: [front] 디렉토리 나누기

---
+++ front/src/screen/ChattingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import UtilButton from '../util/UtilButton';
+
+const ChattingScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Chattings</Text>
+      <UtilButton height={50} width={150} content={'유틸 버튼 테스트용.'} />
+    </View>
+  );
+};
+
+export default ChattingScreen;
