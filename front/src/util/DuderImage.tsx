6a2654192acd46b9838826a8109ae3d305090a0e
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-13 02:48:05 +0900

    feat: [front] 듀더 이미지 컴포넌트 (#8)

---
+++ front/src/util/DuderImage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import DUDER from '../assets/images/DUDER_IMAGE.png';
+
+const DuderImage = () => {
+  return (
+    <Wrapper>
+      <AppImage source={DUDER}></AppImage>
+    </Wrapper>
+  );
+};
+
+export default DuderImage;
+
+const Wrapper = styled.View`
+  width: 200;
+  height: 200;
+  background-color: red;
+`;
+
+const AppImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
