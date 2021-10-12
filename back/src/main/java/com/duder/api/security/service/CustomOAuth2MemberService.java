b7dd36865a2ca30508c4019f0312047caafaca9e
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-13 02:43:01 +0900

    feat: [back] 소셜 로그인 성공 시 PrincipalDetail 객체 만들어 반환 (#8)

--- back/src/main/java/com/duder/api/security/service/CustomOAuth2MemberService.java
+++ back/src/main/java/com/duder/api/security/service/CustomOAuth2MemberService.java
@@ -4,6 +4,7 @@ import com.duder.api.member.domain.Member;
 import com.duder.api.member.domain.MemberRepository;
 import com.duder.api.member.domain.Role;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -12,8 +13,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.LinkedHashMap;
+import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2MemberService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
@@ -29,27 +31,17 @@ public class CustomOAuth2MemberService implements OAuth2UserService<OAuth2UserRe
 
         // 서비스 id(카카오, 구글 등)
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        log.info("success Authentication: " + registrationId);
+        ProviderType providerType = ProviderType.valueOf(registrationId.toUpperCase());
 
-        // 로그인 진행 시 키가 되는 필드 값 (PK)
-        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
-            .getUserInfoEndpoint().getUserNameAttributeName();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        OAuthAttribute oAuthAttribute = OAuthAttribute.of(providerType, attributes);
 
+        Member member = memberRepository.findByProviderId(oAuthAttribute.getProviderId())
+                .orElseGet(() -> memberRepository.save(oAuthAttribute.toEntity()));
 
-        System.out.println("registrationId = " + registrationId);
-        System.out.println("userNameAttributeName = " + userNameAttributeName);
+        log.info("loadUser member = " + member);
 
-        String kakaoId = oAuth2User.getName();
-        LinkedHashMap<String, Object> kakao_account = (LinkedHashMap<String, Object>) oAuth2User.getAttributes().get("kakao_account");
-        String email = (String) kakao_account.get("email");
-
-        System.out.println("kakaoId = " + kakaoId);
-
-        Member member = memberRepository.findByKakaoId(kakaoId)
-                .orElseGet(() -> memberRepository.save(
-                        new Member(kakaoId, null, null, Role.USER, null, email, null)));
-
-        System.out.println("loadUser member = " + member);
-
-        return oAuth2User;
+        return new PrincipalDetail(member, attributes);
     }
 }
