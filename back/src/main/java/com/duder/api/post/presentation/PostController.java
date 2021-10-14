5a5e163ef8b8b42de6530d66f76a570b598d0675
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-15 01:07:45 +0900

    feat: [back] 게시글 에러처리 및 api 추가 (#5)

--- back/src/main/java/com/duder/api/post/presentation/PostController.java
+++ back/src/main/java/com/duder/api/post/presentation/PostController.java
@@ -1,8 +1,8 @@
 package com.duder.api.post.presentation;
 
 import com.duder.api.form.ApiForm;
-import com.duder.api.member.domain.Member;
 import com.duder.api.post.request.PostEnrollRequest;
+import com.duder.api.post.request.PostUpdateRequest;
 import com.duder.api.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,21 +18,24 @@ import static com.duder.api.form.ApiForm.succeed;
 public class PostController {
 
     private final PostService postService;
+    public final String SUCCESS_ENROLL_POST = "게시글 등록에 성공했습니다.";
+    public final String SUCCESS_FIND_POST = "게시글을 조회에 성공했습니다.";
+    public final String SUCCESS_UPDATE_POST = "게시글을 수정했습니다.";
+    public final String SUCCESS_DELETE_POST = "게시글을 삭제했습니다.";
 
     @PostMapping("/enroll")
     public ApiForm<?> enroll(@AuthenticationPrincipal OAuth2User oAuth2User, PostEnrollRequest request){
         try {
-            return succeed(postService.enroll(oAuth2User.getAttribute("member"), request), "게시글 등록에 성공했습니다.");
+            return succeed(postService.enroll(oAuth2User.getAttribute("member"), request), SUCCESS_ENROLL_POST);
         } catch (Exception e) {
             return fail(e.getMessage());
         }
     }
 
     @GetMapping("/{postId}")
-    public ApiForm<?> findPostById(@AuthenticationPrincipal OAuth2User oAuth2User, @PathVariable Long postId)
-            throws IllegalArgumentException{
+    public ApiForm<?> findPostById(@PathVariable Long postId) {
         try {
-            return succeed(postService.findPostById(postId), "게시글을 찾았습니다.");
+            return succeed(postService.findPostById(postId), SUCCESS_FIND_POST);
         }catch (IllegalArgumentException e){
             return fail(e.getMessage());
         }
@@ -40,9 +43,27 @@ public class PostController {
 
     @GetMapping("/get")
     public ApiForm<?> findPostsByDistance(@RequestParam("latitude") double latitude, @RequestParam("longitude") double longitude,
-                                          @RequestParam("distance") int distance) throws IllegalArgumentException{
+                                          @RequestParam("distance") int distance){
         try{
-            return succeed(postService.findPostsByDistance(latitude, longitude, distance), "게시글을 찾았습니다.");
+            return succeed(postService.findPostsByDistance(latitude, longitude, distance), SUCCESS_FIND_POST);
+        }catch (IllegalArgumentException e){
+            return fail(e.getMessage());
+        }
+    }
+
+    @PutMapping("/update/{postId}")
+    public ApiForm<?> updatePost(@PathVariable Long postId, @RequestBody PostUpdateRequest postUpdateRequest){
+        try {
+            return succeed(postService.updatePost(postId, postUpdateRequest), SUCCESS_UPDATE_POST);
+        }catch (IllegalArgumentException e){
+            return fail(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete/{postId}")
+    public ApiForm<?> deletePost(@AuthenticationPrincipal OAuth2User oAuth2User, @PathVariable Long postId){
+        try{
+            return succeed(postService.deletePost(oAuth2User.getAttribute("member"), postId), SUCCESS_DELETE_POST);
         }catch (IllegalArgumentException e){
             return fail(e.getMessage());
         }
