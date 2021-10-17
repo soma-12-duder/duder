ef53e6d792817c057c7e5534a9e3e210856a64d7
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-17 14:55:54 +0900

    feat: [back] 기존 로직 -> row, column을 반환하게 변경 (#13)

---
+++ back/src/main/java/com/duder/api/post/service/Coordinate.java
@@ -0,0 +1,13 @@
+package com.duder.api.post.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Coordinate {
+    private Integer row;
+    private Integer column;
+}
