e672c01241aa4b5da7fd190fe024c08b496ce81e
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-29 01:45:34 +0900

    feat: [front] props 사진 추가

--- front/src/components/buttons/LoginButton.tsx
+++ front/src/components/buttons/LoginButton.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-const LoginButton = ({onPress, name}: any) => {
+const LoginButton = ({onPress, name, image}: any) => {
   return (
     <Wrapper>
       <KakaoButton onPress={onPress}>
+        <LogoImage source={image} />
         <ButtonText>{name}</ButtonText>
+        <RemainSpace />
       </KakaoButton>
     </Wrapper>
   );
@@ -14,10 +16,10 @@ const LoginButton = ({onPress, name}: any) => {
 export default LoginButton;
 
 const Wrapper = styled.View`
-  border-radius: 8px;
+  border-radius: 3px;
   width: 100%;
-  height: 48px;
-  background-color: #ffe812;
+  height: 50px;
+  background-color: #ffe500;
   justify-content: center;
   align-items: center;
 `;
@@ -25,11 +27,21 @@ const Wrapper = styled.View`
 const KakaoButton = styled.TouchableOpacity`
   width: 100%;
   height: 100%;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
+  flex-direction: row;
 `;
 
 const ButtonText = styled.Text`
   font-size: 16px;
   font-weight: 700;
+`;
+
+const LogoImage = styled.Image`
+  width: 17px;
+  height: 16px;
+`;
+
+const RemainSpace = styled.View`
+  width: 17px;
 `;
