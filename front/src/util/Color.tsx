6ae1a473ff18b24706e06a8e51e357eb0acf6866
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-29 01:47:39 +0900

    feat: [front] 유틸성 컴포넌트 추가

---
+++ front/src/util/Color.tsx
@@ -0,0 +1,1 @@
+export const BROWN = '#c38753';
--- front/src/util/UtilButton.tsx
+++ front/src/util/UtilButton.tsx
@@ -4,20 +4,35 @@ import {Text} from 'react-native';
 
 interface Style {
   height: number;
-  width: number;
+  width: string | number;
+  backgroundColor: string;
 }
 
 interface Props {
+  onPress: any;
   height: number;
-  width: number;
+  width: string | number;
   content: any;
+  color: string;
+  backgroundColor: string;
 }
 
-const UtilButton = ({height = 50, width = 100, content}: Props) => {
+interface Content {
+  color: string;
+}
+
+const UtilButton = ({
+  onPress,
+  height = 50,
+  width = '100%',
+  backgroundColor = 'white',
+  color = 'black',
+  content,
+}: Props) => {
   return (
-    <Wrapper height={height} width={width}>
-      <Button>
-        <Text>{content}</Text>
+    <Wrapper height={height} width={width} backgroundColor={backgroundColor}>
+      <Button onPress={onPress}>
+        <ButtonContent color={color}>{content}</ButtonContent>
       </Button>
     </Wrapper>
   );
@@ -28,10 +43,19 @@ export default UtilButton;
 const Wrapper = styled.View`
   height: ${(props: Style) => props.height};
   width: ${(props: Style) => props.width};
-  background-color: red;
+  background-color: ${(props: Style) => props.backgroundColor};
+  border-radius: 3px;
 `;
 
 const Button = styled.TouchableOpacity`
   height: 100%;
   width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonContent = styled.Text`
+  font-size: 15px;
+  letter-spacing: -0.3;
+  color: ${(props: Content) => props.color};
 `;
