05744313d03d7845935818e1f434f7fe8342650a
lee-seungbeom <lee.seungbeom@navercorp.com>
2021-11-01 01:05:31 +0900

    댓글화면 추가

--- front/src/components/ViewCommentText.tsx
+++ front/src/components/ViewCommentText.tsx
@@ -8,21 +8,38 @@ import {Colors} from '../util/Constants';
 
 interface Props {
   comment: String;
-  commentOfComment: boolean;
+  nickname: String;
+  like: String;
+  commentOfComment?: boolean;
 }
 
-const ViewCommentText = ({comment, commentOfComment}: Props) => {
+const ViewCommentText = ({
+  comment,
+  commentOfComment,
+  nickname,
+  like,
+}: Props) => {
   return (
-    <>
+    <CommentView commentOfComment={commentOfComment}>
       <ProfileWrapper>
         <UserProfileImage
           source={{
             uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ebn2o15gmobO1xOj1ESvldLkPBxnC4ZwDg&usqp=CAU',
           }}
         />
-        <Text style={{fontSize: 20}}>fumyparli</Text>
+        <Text style={{fontSize: 16, paddingLeft: '1.5%'}}>{nickname}</Text>
       </ProfileWrapper>
-    </>
+      <Text style={{paddingLeft: '4%'}}>{comment}</Text>
+      <GrayTextWrapper>
+        <TouchableOpacity>
+          <GrayText>좋아요</GrayText>
+        </TouchableOpacity>
+        <GrayText style={{paddingLeft: '1%'}}>{like}</GrayText>
+        <TouchableOpacity style={{paddingLeft: '3%'}}>
+          {commentOfComment ? <></> : <GrayText>답글쓰기</GrayText>}
+        </TouchableOpacity>
+      </GrayTextWrapper>
+    </CommentView>
   );
 };
 
@@ -30,13 +47,31 @@ export default ViewCommentText;
 
 const ProfileWrapper = styled(View)`
   flex-direction: row;
-  justify-content: space-between;
   align-items: center;
-  width: 34%;
+  padding-left: 4%;
+  padding-bottom: 2%;
 `;
 
 const UserProfileImage = styled(Image)`
-  width: 35px;
-  height: 35px;
+  width: 25px;
+  height: 25px;
   border-radius: 100px;
+`;
+
+const GrayText = styled(Text)`
+  color: #a9a9a9;
+`;
+
+const GrayTextWrapper = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  padding-left: 4%;
+  padding-top: 2%;
+`;
+
+const CommentView = styled(View)<{commentOfComment: any}>`
+  background-color: ${(props: any) =>
+    props.commentOfComment ? '#F0F0F0' : '#FFFFFF'};
+  padding: 4%;
+  padding-left: ${(props: any) => (props.commentOfComment ? '4%' : '0')};
 `;
--- front/src/screen/PostScreen.tsx
+++ front/src/screen/PostScreen.tsx
@@ -2,26 +2,87 @@
 import * as React from 'react';
 import ViewMainText from '../components/ViewMainText';
 import ViewCommentText from '../components/ViewCommentText';
+import {ScrollView, StyleSheet} from 'react-native';
 
 interface Props {
   route: any;
   navigation: any;
 }
 
+const data = [
+  {
+    commentOfComment: false,
+    comment:
+      '댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글',
+    like: '7',
+    nickname: 'kingjinoh',
+  },
+  {
+    commentOfComment: true,
+    comment:
+      '댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글',
+    like: '7',
+    nickname: 'kingjinoh',
+  },
+  {
+    comment:
+      '댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글',
+    like: '7',
+    nickname: 'kingjinoh',
+  },
+  {
+    commentOfComment: true,
+    comment:
+      '댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글',
+    like: '7',
+    nickname: 'kingjinoh',
+  },
+  {
+    commentOfComment: true,
+    comment:
+      '댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글',
+    like: '7',
+    nickname: 'kingjinoh',
+  },
+  {
+    commentOfComment: false,
+    comment:
+      '댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글',
+    like: '7124324',
+    nickname: 'kingjinoh',
+  },
+  {
+    commentOfComment: true,
+    comment:
+      '댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글',
+    like: '253467',
+    nickname: 'kingjinoh',
+  },
+];
+
 const PostScreen = ({route, navigation}: Props) => {
   return (
     <>
-      <ViewMainText text={route.params.text} />
-      <ViewCommentText
-        commentOfComment={false}
-        comment="댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글"
-      />
-      <ViewCommentText
-        commentOfComment={true}
-        comment="대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글 대댓글"
-      />
+      <ScrollView contentContainerStyle={styles.container}>
+        <ViewMainText text={route.params.text} />
+        {data.map(item => (
+          <ViewCommentText
+            commentOfComment={item.commentOfComment}
+            comment={item.comment}
+            like={item.like}
+            nickname={item.nickname}></ViewCommentText>
+        ))}
+      </ScrollView>
     </>
   );
 };
 
 export default PostScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+});
