3e8a40720e4abab66cc2f4522b7e1f0031584f52
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-11-17 14:53:11 +0900

    feat: [front] 색 관련 상수화 (#7)

--- front/src/util/Color.tsx
+++ front/src/util/Color.tsx
@@ -1,1 +1,6 @@
 export const BROWN = '#c38753';
+export const LIGHT_GRAY = '#eaebef';
+export const GRAY = '#757575';
+// export const LIGHT_GRAY = '#991e0d06';
+export const NK700 = 'NotoSansKR-Bold';
+export const NK500 = 'NotoSansKR-Medium';
