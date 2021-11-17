ee7fa47e4cd42cfdac48314c771d4945134eb5da
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-11-17 14:55:00 +0900

    feat: [front] 채팅목록 화면 구현 (#7)

--- front/src/screen/ChattingScreen.tsx
+++ front/src/screen/ChattingScreen.tsx
@@ -1,117 +1,81 @@
 /* eslint-disable react-native/no-inline-styles */
-import * as React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
 import HorizontalLine from '../components/HorizontalLine';
 import Chatting from '../components/Chatting';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {chatListState} from '../states/ChatState';
 
-const data = [
-  {
-    nickname: 'fumyparli',
-    text: '안녕하세요~~~~~~~~!!!!!!!!!!',
-    date: '1월 30일',
-    km: '3.4',
-    chattingNumber: '3',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '뿌에에에에엥뿌에에에에엥뿌에에에에엥뿌에에에에엥뿌에에에에엥뿌에에에에엥뿌에에에에엥뿌에에에에엥뿌에에에에엥뿌에에에에엥',
-    date: '1월 28일',
-    km: '3',
-    chattingNumber: '2',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '나랑놀자~~~~~~~~~~~~~!@~@!@!@~~~~~!!!!!!!!!!!!!!!!!!!',
-    date: '1월 27일',
-    km: '2',
-    chattingNumber: '1235',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅채팅',
-    date: '1월 26일',
-    km: '3',
-    chattingNumber: '9',
-  },
-  {
-    nickname: 'fumyparli',
-    text: 'ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ',
-    date: '1월 25일',
-    km: '7',
-    chattingNumber: '5',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '뭐해??~~~~~~~~!!!!!!!!!!',
-    date: '1월 25일',
-    km: '3',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '안녕하십니까~~~~~~~~!!!!!!!!!!',
-    date: '1월 25일',
-    km: '9',
-    chattingNumber: '9',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '!!!!!!',
-    date: '1월 23일',
-    km: '0.1',
-    chattingNumber: '1',
-  },
-  {
-    nickname: 'fumyparli',
-    text: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
-    date: '1월 22일',
-    km: '10',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '뭐해??~~~~~~~~!!!!!!!!!!',
-    date: '1월 25일',
-    km: '3',
-    chattingNumber: '2',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '안녕하십니까~~~~~~~~!!!!!!!!!!',
-    date: '1월 25일',
-    km: '9',
-    chattingNumber: '2',
-  },
-  {
-    nickname: 'fumyparli',
-    text: '!!!!!!',
-    date: '1월 23일',
-    km: '0.1',
-  },
-  {
-    nickname: 'fumyparli',
-    text: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
-    date: '1월 22일',
-    km: '10',
-  },
-  {
-    nickname: 'fumyparli',
-    text: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
-    date: '1월 22일',
-    km: '10',
-  },
-];
+import {chatApi} from '../api/indexApi';
+import {memberInfoState} from '../states/MemberState';
+import {useFocusEffect, useNavigation} from '@react-navigation/core';
 
 const ChattingScreen = () => {
+  const navigation: any = useNavigation();
+  const memberInfo = useRecoilValue(memberInfoState);
+  const [chatList, setChatList] = useRecoilState(chatListState);
+  const [lastMessageList, setLastMessageList]: any = useState([]);
+
+  const getData = async () => {
+    const {status, data}: any = await chatApi.findAllChatrooms();
+    if (status === 200) {
+      // console.log('###### 채팅 전체 목록 데이터: ', data);
+      setChatList(data);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      const getLastMessage = async (room_id: any) =>
+        await chatApi.findLastChatMessage(room_id);
+
+      const initChatLastMessage = async () => {
+        const {data} = await chatApi.findAllChatrooms();
+        setChatList(data);
+        console.log('init Last Message');
+        setLastMessageList(
+          await Promise.all(
+            data.map(async (chatroom: any, i: any) => {
+              const {data: lastMessage} = await getLastMessage(
+                chatroom.chatroom_id,
+              );
+              return lastMessage;
+            }),
+          ),
+        );
+      };
+
+      initChatLastMessage();
+
+      const polling = setInterval(initChatLastMessage, 25 * 1000);
+
+      return () => clearInterval(polling);
+    }, []),
+  );
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const onClick = (nickname: String, chatroom_id: String) =>
+    navigation.navigate('ChatMessageScreen', {nickname, chatroom_id});
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <HorizontalLine />
-      {data.map(item => (
-        <Chatting
-          nickname={item.nickname}
-          text={item.text}
-          date={item.date}
-          km={item.km}
-          chattingNumber={item.chattingNumber}></Chatting>
-      ))}
+      {chatList.length > 0 &&
+        lastMessageList.length > 0 &&
+        chatList.map(({chatroom_id, member, opponent}: any, i: any) => (
+          <Chatting
+            key={chatroom_id}
+            opponent={memberInfo.id === member.id ? opponent : member}
+            chatroom_id={chatroom_id}
+            date={lastMessageList[i].created_at}
+            content={lastMessageList[i].content}
+            km={'3'}
+            onClick={onClick}
+          />
+        ))}
     </ScrollView>
   );
 };
