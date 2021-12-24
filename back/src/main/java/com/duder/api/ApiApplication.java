6ea5cfcaacf6c8dcc8d8547d1e1b5fe046785e74
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-12-25 01:30:18 +0900

    refactor: [test] main annotation config로 분리

--- back/src/main/java/com/duder/api/ApiApplication.java
+++ back/src/main/java/com/duder/api/ApiApplication.java
@@ -1,14 +1,8 @@
 package com.duder.api;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.persistence.EntityManager;
-
-@EnableJpaAuditing
 @SpringBootApplication
 public class ApiApplication {
 
@@ -16,8 +10,4 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
-	@Bean
-	public JPAQueryFactory jpaQueryFactory(EntityManager em){
-		return new JPAQueryFactory(em);
-	}
 }
